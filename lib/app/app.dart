09bce8934979c0e8f73083aebd43b4fbdebc1a95
Yasser El-Tantawi <yasserbadi4@gmail.com>
2022-11-08 14:30:09 +0200

    lecture 9 finished

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_mina/presentation/theme_manager.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
@@ -17,7 +18,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(theme: getApplicationTheme(),);
   }
 }
 
---
+++ lib/presentation/theme_manager.dart
@@ -0,0 +1,27 @@
+import 'package:ecommerce_mina/presentation/color_manager.dart';
+import 'package:ecommerce_mina/presentation/values_manager.dart';
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    // main colors
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.lightPrimary,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+
+    // card view theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
+    // app bar theme
+
+    // button theme
+
+    // text theme
+
+    // input decoration theme (text form field)
+  );
+}
